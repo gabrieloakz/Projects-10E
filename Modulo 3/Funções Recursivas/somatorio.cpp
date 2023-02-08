79f2b505fd75ee886b134918103f883030446b8f
gabrieloakz <ferreiragabriel0193@gmail.com>
2023-02-08 10:27:40 +0000

    fiz coisas recursivas

---
+++ Modulo 3/Funções Recursivas/somatorio.cpp
@@ -0,0 +1,38 @@
+// gabriel ferreira carvalho 10
+
+#include <iostream>
+#include <locale>
+using namespace std;
+
+int num;
+
+int somatorio(int num)
+{
+
+int x ;
+
+if (num == 1)
+
+    return (1);
+
+
+return num + somatorio(num - 1) ;
+
+}
+
+int main(int argc, char const *argv[])
+{
+
+int resultado ;
+
+    setlocale(LC_ALL, "pt_PT.utf8");
+
+    puts("Insira um Número:") ;
+    cin >> num;
+
+    resultado = somatorio(num) ;
+
+    cout << resultado << endl ;
+
+    return 0;
+}
